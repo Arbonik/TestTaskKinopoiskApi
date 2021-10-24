e61fc38d6e263c04e2bb38d70dc33090791c50f1
arbonik <zmear@mail.ru>
2021-10-25 03:53:53 +0700

    token up

--- app/src/main/java/com/arbonik/soft_logic_test/network/MoviesReference.kt
+++ app/src/main/java/com/arbonik/soft_logic_test/network/MoviesReference.kt
@@ -11,7 +11,10 @@ import retrofit2.http.Path
 //const val TOKEN = "b819ee93ba4e622c897b56e009c7bf3c"
 //const val TOKEN = "5ead8e9e555e01d132f89c0745561c5a" // 25.10 1:12
 //const val TOKEN = "049caf7d58c9a953e9c34fc7cefbddb6" // 25.10 2:30
-const val TOKEN = "d720c0c8702de29fa7bc5fc880b2386a"
+//const val TOKEN = "d720c0c8702de29fa7bc5fc880b2386a" //25.10 3:40
+//const val TOKEN = "f87c91e6a0ecd8ab6fbcc1c30947e285" // 25.10 3:50
+//const val TOKEN = "ac3ffb39c876fd7d50d5b52843b0aef4" // 25.10 3:53
+const val TOKEN = "3438acad5b4315aae9d007a48a2fdc75"
 
 
 interface MoviesReference {
